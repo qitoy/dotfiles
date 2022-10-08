07cb020ef2f06273d4992868a573a24df38bab91
qitoy <su.kan@icloud.com>
2022-10-09 03:20:16 +0900

    add proconinit

--- .vim/denops/procon/main.ts
+++ .vim/denops/procon/main.ts
@@ -1,14 +1,36 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.8.2/mod.ts";
+import { map } from "https://deno.land/x/denops_std@v3.8.2/mapping/mod.ts";
+import { deletebufline, setbufline } from "https://deno.land/x/denops_std@v3.8.2/function/mod.ts";
 import { assertString, ensureObject } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 import { templateCpp } from "./cpp.ts";
 
 export async function main(denops: Denops): Promise<void> {
     denops.dispatcher = {
-        async debug(url: unknown): Promise<unknown> {
+        async proconPrepare(url: unknown): Promise<void> {
             assertString(url);
-            return await proconPrepare(url);
+            const contest = await getContest(url);
+            await prepareDir(contest);
+            denops.cmd("echo 'dir prepared'");
+            await downloadTest(contest);
+            denops.cmd("echo 'test downloaded'");
+        },
+        async proconInit(): Promise<void> {
+            await deletebufline(denops, "", 1, "$");
+            await setbufline(denops, "", 1, templateCpp.split("\n"));
         },
     };
+    await denops.cmd(
+        `command! -nargs=1 ProconPrepare call denops#notify("${denops.name}", "proconPrepare", [<f-args>])`
+    );
+    await map(
+        denops,
+        "<Plug>(procon-init)",
+        `<Cmd>call denops#notify("${denops.name}", "proconInit", [])<CR>`,
+        {
+            mode: "n",
+            noremap: true,
+        },
+    );
 }
 
 type Problem = {
@@ -23,12 +45,26 @@ type Contest = {
     problems: Problem[];
 };
 
-async function proconPrepare(url: string): Promise<void> {
-    const contest = await getContest(url);
+async function prepareDir(contest: Contest): Promise<void> {
+    Deno.mkdirSync(contest.name);
+    await Promise.all(contest.problems.map(async (problem: Problem) => {
+        const problemDir = `${contest.name}/${problem.context.alphabet}`;
+        await Deno.mkdir(problemDir);
+        return Deno.writeTextFile(`${problemDir}/main.cpp`, templateCpp);
+    }));
+}
+
+async function downloadTest(contest: Contest): Promise<void> {
     for(const problem of contest.problems) {
-        await Deno.mkdir(`${contest.name}/${problem.context.alphabet}`, { recursive: true });
-        await Deno.writeTextFile(`${contest.name}/${problem.context.alphabet}/main.cpp`, templateCpp);
-        await Deno.writeTextFile(`${contest.name}/${problem.context.alphabet}/.contest_url`, problem.url);
+        const problemDir = `${contest.name}/${problem.context.alphabet}`;
+        const p = Deno.run({
+            cmd: ["oj", "download", "-d", `${contest.name}/${problem.context.alphabet}/test/`, problem.url],
+            stdin: "null",
+            stdout: "null",
+            stderr: "null",
+        });
+        await Deno.writeTextFile(`${problemDir}/.contest_url`, problem.url);
+        await p.status();
     }
 }
 
