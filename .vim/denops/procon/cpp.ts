ae8677ada916d930a2c356299dfea3d273822e1b
qitoy <su.kan@icloud.com>
2022-11-06 17:43:23 +0900

    fix

--- .vim/denops/procon/cpp.ts
+++ .vim/denops/procon/cpp.ts
@@ -1,10 +1,10 @@
 export const templateCpp = `/*{{{ begin template */
 #include <bits/stdc++.h>
+using namespace std; using ll = long long;
 template <class T> inline bool chmin(T &a, T b) { if (a > b) { a = b; return true; } return false; }
 template <class T> inline bool chmax(T &a, T b) { if (a < b) { a = b; return true; } return false; }
-template <class T> std::istream &operator>>(std::istream &is, std::vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
-void Main(); int main() { std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false); std::cout << std::fixed << std::setprecision(15); Main(); }
-using namespace std; using ll = long long;
+template <class T> istream &operator>>(istream &is, vector<T> &vec) { for (auto&& v : vec) is >> v; return is; }
+void Main(); int main() { cin.tie(nullptr); ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); Main(); }
 /*}}} end template */
 
 void Main() {
