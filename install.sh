dd06d98c5d93a85f765483e7f188620b08572a9d
qitoy <su.kan@icloud.com>
2022-11-18 09:39:10 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# install homebrew
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# clone dotfiles
+if [ ! -d $HOME/dotfiles ]; then
+    git clone https://github.com/qitoy/dotfiles $HOME/dotfiles
+fi
+
 # dotfiles
 dotfiles=("vimrc" "vim" "latexmkrc" "zshrc" "zprofile")
 for dotfile in "${dotfiles[@]}"; do
