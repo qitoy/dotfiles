c8e92503b8a8e01741e7783c7518f230b143ec24
qitoy <itmkan@icloud.com>
2022-12-25 05:34:27 +0900

    [zsh] fix

--- zshrc/common.zsh
+++ zshrc/common.zsh
@@ -13,15 +13,12 @@ export MOCWORD_DATA=$HOME/.mocword/mocword.sqlite
 source $HOME/.secret
 
 ### Added by Zinit's installer
-if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
-    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
-    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
-        print -P "%F{33} %F{34}Installation successful.%f%b" || \
-        print -P "%F{160} The clone has failed.%f%b"
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+if [[ ! -f ${ZINIT_HOME}/zinit.zsh ]]; then
+    mkdir -p "$(dirname $ZINIT_HOME)"
+    git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 fi
-
-source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+source "${ZINIT_HOME}/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
@@ -41,12 +38,6 @@ setopt magic_equal_subst
 # setopt correct
 
 # autoload
-if type brew &>/dev/null; then
-    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-
-    autoload -Uz compinit
-    compinit
-fi
 autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
 add-zsh-hook chpwd chpwd_recent_dirs
 zstyle 'chpwd:*' recent-dirs-max 5000
@@ -64,9 +55,5 @@ select-word-style bash
 # alias
 alias cdr="anyframe-widget-cdr"
 
-[ -f "$HOME/.ghcup/env" ] && source "$HOME/.ghcup/env" # ghcup-env
-
 # powerline
 . /usr/lib/python3.10/site-packages/powerline/bindings/zsh/powerline.zsh
-
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
