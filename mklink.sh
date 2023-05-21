6babc3aac02c0a00a81760c89819de3c37e6b9bd
qitoy <itmkan@icloud.com>
2023-05-21 22:03:40 +0900

    symlinkを貼るのをシェルスクリプトにした

---
+++ mklink.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+set -ueC
+
+local -a dots
+local -a configs
+local -a symlinks
+
+dot_path=$(cd $(dirname $0) && pwd)
+
+dots+=("latexmkrc" "w3m" "vim" "vimrc" "zprofile" "zshrc" "gitmessage")
+configs+=("wezterm" "powerline-shell" "git")
+symlinks+=("$dot_path/vim $HOME/.config/nvim")
+
+case $OSTYPE in
+    darwin*)
+        dots+=("hammerspoon")
+        configs+=("karabiner");;
+    linux*)
+        configs+=("systemd" "i3" "sway" "swaylock" "waybar");;
+esac
+
+for conf in $configs; do dots+=("config/$conf"); done
+for dot in $dots; do symlinks+=("$dot_path/$dot $HOME/.$dot"); done
+
+for sym in $symlinks; do eval "ln -nsfv $sym"; done
