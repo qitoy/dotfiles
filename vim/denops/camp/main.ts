58f2b922676fddc723c62c3e965a82060eaa0588
qitoy <itmkan@icloud.com>
2023-12-31 23:05:02 +0900

    update camp

--- vim/denops/camp/main.ts
+++ vim/denops/camp/main.ts
@@ -1,8 +1,4 @@
-import { $ } from "https://deno.land/x/dax@0.35.0/mod.ts";
-import { systemopen } from "https://deno.land/x/systemopen@v0.2.0/mod.ts";
-import { type Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
-import { assert, is } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
+import { $, Denops, fn, is, systemopen, u } from "../deps.ts";
 
 export function main(denops: Denops): void {
   denops.dispatcher = {
@@ -19,13 +15,8 @@ export function main(denops: Denops): void {
       await denops.call("camp#write", "test", output);
     },
 
-    async campTestJson(): Promise<void> {
-      const json = await competeTestJson(await currentFullPath(denops));
-      console.log(json);
-    },
-
     async campSubmit(force: unknown): Promise<void> {
-      assert(force, is.Number);
+      u.assert(force, is.Number);
       const source = await currentFullPath(denops);
       if (!force) {
         const [success, output] = await competeTest(source);
@@ -70,17 +61,6 @@ async function competeTest(source: string): Promise<[boolean, string[]]> {
     .noThrow()
     .spawn();
   return [code === 0, combined.split("\n")];
-}
-
-async function competeTestJson(source: string): Promise<any> {
-  const path = $.path(source);
-  const dir = path.dirname();
-  const name = path.basename().slice(0, -3);
-  return await $`cargo compete test ${name} --json`
-    .quiet("stderr")
-    .cwd(dir)
-    .noThrow()
-    .json();
 }
 
 async function competeSubmit(source: string): Promise<string> {
