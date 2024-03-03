b696463fcc5df834cca2e915029a8ef7f7acb50e
qitoy <itmkan@icloud.com>
2024-03-04 03:44:28 +0900

    use float term

---
+++ config/sway/float_term.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -u
+
+float_term=$(swaymsg -t get_tree | jq -c '.. | .floating_nodes? | arrays[] | select(.app_id=="float_term")')
+
+if [ -z $float_term ]; then
+  wezterm --config initial_rows=60\
+    --config initial_cols=200\
+    start --class float_term
+else
+  if [ "$(echo $float_term | jq .focused)" = 'true' ]; then
+    swaymsg "move window to scratchpad"
+  else
+    swaymsg "[app_id=\"float_term\"] focus"
+  fi
+fi
